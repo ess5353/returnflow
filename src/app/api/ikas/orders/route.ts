d306a05f8bcd5f4b52b97ff5a49f483d94116afa
Eyüp Sina Sirkeci <ess@Eyup-MacBook-Air.local>
2026-06-25 16:28:12 +0300

    show ikas queries

--- src/app/api/ikas/orders/route.ts
+++ src/app/api/ikas/orders/route.ts
@@ -26,7 +26,7 @@ export async function GET(request: NextRequest) {
     }
 
     const ikas = getIkas(authToken);
-
+console.log(Object.keys(ikas.queries));
     const response = await ikas.queries.listOrder();
 
     const orders =
