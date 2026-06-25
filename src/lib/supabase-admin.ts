95666764082c69cdb010faf968f59670efac92e9
Eyüp Sina Sirkeci <ess@Eyup-MacBook-Air.local>
2026-06-25 15:34:04 +0300

    debug supabase connection

--- src/lib/supabase-admin.ts
+++ src/lib/supabase-admin.ts
@@ -1,5 +1,15 @@
 import { createClient } from '@supabase/supabase-js';
 
+console.log(
+  'SUPABASE URL:',
+  process.env.NEXT_PUBLIC_SUPABASE_URL
+);
+
+console.log(
+  'SERVICE KEY EXISTS:',
+  !!process.env.SUPABASE_SERVICE_ROLE_KEY
+);
+
 export const supabaseAdmin = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!
